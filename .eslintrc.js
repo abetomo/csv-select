682abbebbafbeb1ff9e9c3db6f6a614dbe3d9fbe
abetomo <abe@enzou.tokyo>
2021-07-07 12:25:22 +0900

    fix(eslint): update .eslintrc.js
    https://github.com/prettier/eslint-config-prettier/blob/main/CHANGELOG.md#version-800-2021-02-21

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,6 @@ module.exports = {
   extends: [
     '@nuxtjs/eslint-config-typescript',
     'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
   ],
   plugins: ['prettier'],
   // add your custom rules here
